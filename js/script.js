1711be8847855cbe60d366f3e4e5f34010d6054e
Serhii Kononov <sergei97k@gmail.com>
2023-09-07 21:16:21 +0300

    added function multiply

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@ function sum(a, b) {
   return a + b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 let a = 5;
 let b = 10;
 console.log(5);
 console.log(sum(a, b));
 console.log(sum(a, 15));
+console.log(multiply(a, b));
